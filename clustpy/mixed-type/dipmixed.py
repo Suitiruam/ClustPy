4f90c845a6185459ae16d38ee9d1e6c4f868cfa6
Suitiruam <108340341+Suitiruam@users.noreply.github.com>
2023-05-03 09:18:39 +0200

    Update dipmixed.py

--- clustpy/mixed-type/dipmixed.py
+++ clustpy/mixed-type/dipmixed.py
@@ -8,8 +8,9 @@ from scipy.spatial.distance import pdist, squareform
 from clustpy.utils import dip_test, dip_pval, dip_boot_samples
 from sklearn.cluster import KMeans
 from sklearn.utils import check_random_state
+import itertools as it
 
-def _dipmixed(X: np.ndarray, numerical_dims:[int], categorical_dims: [int], n_clusters_init: int, random_state: np.random.RandomState) -> (int, np.ndarray, np.ndarray):
+def _dipmixed(X: np.ndarray, numerical_dims:[int], categorical_dims: [int], k: int, p_value_threshold: float , random_state: np.random.RandomState) -> (int, np.ndarray, np.ndarray):
     """
     Start the actual DipMixed clustering procedure on the input data set.
 
@@ -31,32 +32,47 @@ def _dipmixed(X: np.ndarray, numerical_dims:[int], categorical_dims: [int], n_cl
         The labels as identified by DipMeans,
         The cluster centers as identified by DipMeans
     """
-    X_num= X[:, numerical_dims]
-    X_cat= X[:, categorical_dims]
+    X_num= X[:,numerical_dims]
+    X_cat= X[:,categorical_dims]
 
-    X_num_cspace, X_num_nspace= checkUnimodality(X_num)
-    k= determine_number_of_cluster(X_num_cspace) #Maybe based both on numerical and categorical features
+    X_num_cspace, X_num_nspace, weights= checkUnimodality(X_num,numerical_dims,p_value_threshold)
+    if k== None:
+        k= determine_number_of_cluster(X_num_cspace) #Maybe based both on numerical and categorical features
 
-    X_final= map_categorial_to_numerical(X_num_cspace,X_cat, k)
+    X_final, feature_weights= map_categorial_to_numerical(X_num_cspace,X_cat, k, weights,numerical_dims,categorical_dims)
 
     kmeans = KMeans(n_clusters=k, random_state=0, n_init="auto").fit(X_final)
 
     return k, kmeans.labels_, kmeans.cluster_centers_
 
-def checkUnimodality(X_num):
+def checkUnimodality(X_num, numerical_dims, p_value_threshold):
+    clustered_features = []
+    noise_features = []
+    for num_dims in numerical_dims:
+        dip_value=dip_test(X_num[:num_dims])
+        pval = dip_pval(dip_value, np.shape(X_num)[0])
+        if pval < p_value_threshold:
+            clustered_features.add(num_dims)
+        else:
+            noise_features.add(num_dims)
+    X_num_cspace=X_num[:,clustered_features]
+    X_num_nspace=X_num[:,noise_features]
     return X_num_cspace, X_num_nspace
+
+
 
 def determine_number_of_cluster(X_num_cspace):
     return k
 
-def map_categorical_to_numerical(X_num_cspace,X_cat,k):
-    for cat_dim in :
+def map_categorial_to_numerical(X_num_cspace,X_cat,k, feature_weights, numerical_dims,categorical_dims):
+    for cat_dim in categorical_dims:
         num_options=[]
-        for num_dim in :
+        for num_dim in numerical_dims:
             uni_modal=False
-            for cat_attr in :
-                projected_X=
-                dip_value, p_value= checkUnimodality(projected_X)
+            for cat_attr in np.unique(X_cat[:,cat_dim]):
+                X_cat_rows = np.where(X_cat[:cat_dim] == (cat_attr))
+                X_reduced = X_num_cspace[X_cat_rows, :][:, num_dim]
+                dip_value, p_value= checkUnimodality(X_reduced)
                 if is unimodal:
                     unimodal=True
                 else:
@@ -64,24 +80,47 @@ def map_categorical_to_numerical(X_num_cspace,X_cat,k):
                     break
             if unimodal:
                 num_options.add(num_options)
-    for cat_dim in :
-        best_match= [None, 100]
+
+    cat_mapping=[]
+    projections=[]
+
+    for cat_dim in categorical_dims:
+        attribute_combinations= pd.Series(list(it.combinations(np.unique(X_cat[:cat_dim]),2)))
+        best_projection= None
+        best_projection_costs=100
+        projection_weight=0
         for num_option in num_options:
-            X_option=
             costs=0
-            for cat1, cat2 in :
-                dip_value, p_value=checkMultimodality(X_option, X_cat1, X_cat2)
+            for combination in attribute_combinations:
+                cat1, cat2= combination[0],combination[1]
+                X_cat_rows=np.where(X_cat[:cat_dim] == (cat1 or cat2))
+                X_option=X_num_cspace[X_cat_rows,:][:,num_option]
+                dip_value, p_value=checkMultimodality(X_option)
                 if is unimodal:
                     mergeAttributes()
+                    mergeIterator+=1
                 mapping_costs=
                 costs+= mapping_costs
-            if costs/number_of_attribute_combinations < best_match[0][1]:
-                best_match=[num_option,costs]
+            feature_costs= costs/len(attribute_combinations)
+            if feature_costs < best_projection_costs:
+                best_projection = num_option
+                best_projection_costs=feature_costs
+                projection_weight=diff(best_projection_cost,feature_weights[num_option])
             else:
                 continue
+        if len(attributeCombinations)>1:
+            cat_mapping.add(cat_dim)
+            projections.add(best_projection)
+            feature_weights.add(projection_weight*feature_weights[num_option])
+        #else:
+         #   cat_noise.add[cat_dim]
 
+        X_cat_cspace= X_num_cspace[:projections]
+        X_final=X_num_cspace+X_cat_cspace
 
-    return X_final
+        #Idea: calculate k based on both numerical and categorical information -> check if equal, if not use both and choose better one
+
+    return X_final, cat_mapping, projections, feature_weights
 
 
 class DipMixed():
@@ -111,10 +150,11 @@ class DipMixed():
     ----------
     """
 
-    def __init__(self, random_state: np.random.RandomState = None):
+    def __init__(self, random_state: np.random.RandomState = None, p_value_threshold:float=0.01):
         self.random_state = check_random_state(random_state)
+        self.p_value_threshold=p_value_threshold
 
-    def fit(self, X: np.ndarray, y: np.ndarray = None) -> 'DipMixed':
+    def fit(self, X: np.ndarray, numerical_dims:[int], categorical_dims:[int], y: np.ndarray = None) -> 'DipMixed':
         """
         Initiate the actual clustering process on the input data set.
         The resulting cluster labels will be stored in the labels_ attribute.
@@ -131,7 +171,7 @@ class DipMixed():
         self : DipMixed
             this instance of the DipMixed algorithm
         """
-        n_clusters, labels, centers = _dipmixed(X, numerical_dims, categorical_dims, self.n_clusters_init)
+        n_clusters, labels, centers = _dipmixed(X, numerical_dims, categorical_dims, self.n_clusters_init, self.p_value_threshold)
         self.n_clusters_ = n_clusters
         self.labels_ = labels
         self.cluster_centers_ = centers
